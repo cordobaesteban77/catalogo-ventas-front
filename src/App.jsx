94c21ec68fae627661d78ae7e377d071c4209619
Córdoba Esteban Augusto <cordobaesteban77@gmail.com>
2025-09-29 04:27:07 -0300

    instalo react-router-dom, agrego el logo en el nav y creo el componente home y su ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import NavbarApp from './components/NavbarApp'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomeScreen from './pages/HomeScreen'
 
 const App = () => {
   return (
-    <NavbarApp />
+    <BrowserRouter>
+      <NavbarApp />
+      <Routes>
+        <Route path='/' element={<HomeScreen />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/components/NavbarApp.jsx
+++ src/components/NavbarApp.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavbarApp = () => {
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        <Link className='navbar-brand' to="/">
+            <img src="/logo.svg" alt="" />
+        </Link>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
