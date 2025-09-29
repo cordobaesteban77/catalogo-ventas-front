b2588694a89f8a53c3de46b6d566e4551840953e
Córdoba Esteban Augusto <cordobaesteban77@gmail.com>
2025-09-29 06:00:23 -0300

    hago la funcion para traer los productos desde el backend

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const apiUrl = `${import.meta.env.VITE_URL_SERVER}`
 
 const HomeScreen = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+      getProducts()
+    }, [])
+    
+    const getProducts = () => {
+        axios.get(`${apiUrl}/products`).then((res) => { setProducts(res.data.products || [])
+        console.log(res.data.products)})
+        .catch(() => console.log("error al traer productos"))
+    }
   return (
     <div>HomeScreen</div>
   )
